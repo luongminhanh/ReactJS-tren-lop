8badc87ff8dddd229f64997ece26b149f3cfa39d
MinhAnh-haui <luongminhanh.haui@gmail.com>
2023-03-26 20:43:25 +0700

    btvn3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css'
-import Student from './components/testCssModule/Student';
-import Teacher from './components/testCssModule/Teacher';
-import FeatureItem from './components/feature/FeatureItem';
 import FeatureList from './components/feature/FeatureList';
-import Game from './components/tictactoe/Game';
-import Toggle from './components/toggle/Toggle';
-import TestSass from './components/testSass/TestSass';
+import Footer from './components/footer/Footer';
+import NavBar from './components/navbar/NavBar';
+import Slide from './components/slide/Slide';
+// import Student from './components/testCssModule/Student';
+// import Teacher from './components/testCssModule/Teacher';
+// import FeatureItem from './components/feature/FeatureItem';
+// import FeatureList from './components/feature/FeatureList';
+// import Game from './components/tictactoe/Game';
+// import Toggle from './components/toggle/Toggle';
+// import TestSass from './components/testSass/TestSass';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <FeatureList/>
-      <Toggle/>
-      <TestSass/>
-      <Student/>
-      <Teacher/>
+      <Slide/>
+      <Footer/>
     </div>
   );
 }
--- src/components/feature/FeatureList.js
+++ src/components/feature/FeatureList.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import FeatureItem from './FeatureItem'
 import FeatureData from './FeatureData.js'
-import './FeatureList.css'
 
 const FeatureList = () => {
-
   return (
     <div className='container'>
-      <div className='feature-title'>
+      <div className='wrapper'>
         <h1>Feature</h1>
         <span>Some of the features and advantages that we provide for those of you who store data in this Data Warehouse.</span>
-      </div>
-      <div className='feature-list-item'>
-        {FeatureData && FeatureData.map((item) =>
-          <FeatureItem key={item.id} title={item.title} img_main={item.img_main} img_background={item.img_background} passage={item.passage} />
-        )}
+        <div className='feature-list-item grid grid-cols-2 w-100'>
+          {FeatureData && FeatureData.map((item) =>
+            <FeatureItem key={item.id} title={item.title} img_main={item.img_main} img_background={item.img_background} passage={item.passage} />
+          )}
+        </div>
       </div>
     </div>
   )
